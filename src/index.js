60ec740c57fef976ebdf69abe754ea2475421f8d
Halyson <>
2021-07-04 02:23:36 -0300

    Add babel to project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+export default class Pokemon {
+    constructor() {  
+
+      this.width = 20;
+      this.player = [250, 250];
+      this.pokemonCaptureds = [[250, 250]];
+
+      this.init()
+    }
+  
+    init = () => {
+      console.log('init')
+
+    }
+  
+  }
+  
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: 'pokemon.min.js',
+    libraryTarget: 'umd',
+    library: 'Pokemon'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    contentBase: [path.join(__dirname), path.join(__dirname, 'lib')]
+  }
+}
